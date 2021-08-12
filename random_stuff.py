e0bdada6dacb3552263016e932cd1f5b4367f7b0
antschum <schum.antonia@gmail.com>
2021-08-12 07:24:30 +0000

    merge importances and generate count for rf.py

--- random_stuff.py
+++ random_stuff.py
@@ -3,14 +3,28 @@ import pandas as pd
 import pickle
 import time
 import datapreprocessing
+import os
 
 
-# Linear Aug 5
-lin_coefs = pd.read_pickle('linearQuick/Coefs.pkl')
+# merge all rf importances to one df
+path = './rf/coefs'
+
+coefs = pd.DataFrame()
+
+for (dirpath, dirnames, filenames) in os.walk(path):
+    print(filenames)
+    for name in filenames:
+        with open(path+'/'+name, 'rb') as file:
+            df = pickle.load(file)
+            coefs = pd.concat([coefs, df])
+
+coefs.to_pickle('rf/Coefs.pkl')
+
+# create count
 regnet_all = f.help_import_database('data/regnet160_all.pkl')
 
 # start = time.process_time()
-coefs = f.help_pivot_to_df(lin_coefs)
+coefs = f.help_pivot_to_df(coefs)
 # print('help_pivot_to_df', time.process_time() - start)
 # start = time.process_time()
 coefs = f.help_meanSD(coefs)
@@ -22,7 +36,7 @@ summary, percentages = f.help_summary(coefs, regnet_all)
 
 count = f.help_summary_to_count(summary, percentages)
 
-with open('linearQuick/count.pkl', 'wb') as f:  
+with open('rf/count.pkl', 'wb') as f:  
     pickle.dump(count, f)
 
 
